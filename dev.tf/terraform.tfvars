1638e46bb4ea0b3a004dc3586c5abfcaf3e53f92
karty11 <Karthikperumal11@gmail.com>
2024-02-07 18:26:56 +0530

    new files

---
+++ dev.tf/terraform.tfvars
@@ -0,0 +1,6 @@
+sgname = "dev-sg-21"
+cidr = ["0.0.0.0/0"]
+mytag = "dev_env"
+amiid = "ami-0dac6b39723a16e20"
+machinetype = "t2.micro"
+keyname = "Venuskey"
---
+++ dev.tf/variables.tf
@@ -0,0 +1,8 @@
+variable "sgname" {
+  sensitive = true
+}
+variable "cidr" {}
+variable "mytag" {}
+variable "amiid" {}
+variable "machinetype" {}
+variable "keyname" {}
