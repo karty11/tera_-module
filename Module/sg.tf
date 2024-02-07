351805016879cb8d68c3d17111951f133ad19efb
karty11 <Karthikperumal11@gmail.com>
2024-02-07 14:05:54 +0530

    new version

--- Module/sg.tf
+++ Module/sg.tf
@@ -7,6 +7,24 @@ ingress{
   description = "ssh"
   from_port = 22
   to_port = 22
+  protocol = "tcp"g
+  cidr_blocks =  var.cidr
+}
+
+ingress{
+  description = "ssh"
+  from_port = 80
+  to_port = 80
   protocol = "tcp"
   cidr_blocks =  var.cidr
+}
+
+egress{
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks =  var.cidr
+}
+tags{
+  Name=var.sgname
 }
